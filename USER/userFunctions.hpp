550707ab72919e33ba9809f16b5e762d4a0bba86
Salbei <marc.heinze@muenster.de>
2020-06-17 21:10:48 +0200

    intro basics

--- USER/userFunctions.hpp
+++ USER/userFunctions.hpp
@@ -2,3 +2,11 @@
 *   Hier können eigene Funktionen eingebunden werden.
 *   Ist in CfgFunctions included.
 */
+
+class GRAD_USER {
+    class common {
+        file = "USER\functions";
+        
+        class intro {};
+    };
+};
